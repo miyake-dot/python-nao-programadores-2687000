c6600850a67434a96a9ebe39be286bacb6f9cf08
miyake-dot <izabel.miyake@gmail.com>
2024-08-28 15:23:48 +0000

    Resolução numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,16 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
+tipo_data_nascimento = type(data_nascimento)
+tipo_idade_numerica = type(idade_numerica)
+tipo_altura = type(altura)
 
+print(tipo_data_nascimento)
+print(tipo_idade_numerica)
+print(tipo_altura)
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print('nascimento'*4)
 
 # Realize uma operação entre dados do tipo inteiro e float
+print(8+6.5)
